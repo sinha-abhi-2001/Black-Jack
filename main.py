3883b1965889b1009ac555b877acf8c090b0f0f6
sinha-abhi-2001 <sinha00abhishek@gmail.com>
2023-11-13 13:15:39 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 #BLACK JACK 
 import random
-card_values = {
-    'Ace': 1,
-    '2': 2,
-    '3': 3,
-    '4': 4,
-    '5': 5,
-    '6': 6,
-    '7': 7,
-    '8': 8,
-    '9': 9,
-    '10': 10,
-    'Jack': 10,
-    'Queen': 10,
-    'King': 10
-}
-def draw_cards():
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def blackjack_game():
+    user_cards = draw_cards()
+    computer_cards = draw_cards()
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"You'r cards are {user_cards}")
+    print(f"Dealer's card is {computer_cards[0]}")
     pass
+
+def draw_cards():
+    dealed_cards = []
+    for i in range(2):
+        dealed_cards.append(random.choice(cards))
+    return dealed_cards
+def calculate_score(card_list):
+    score = 0
+    for card in card_list:
+        score += card
+    return score
 is_continue = True
 should_continue = input("Do you want to play BlackJack? type y to play and n to exit\n ")
-if should_continue == "n":
+if should_continue == "y":
+    is_continue = True
+elif should_continue == "n":
     is_continue = False
+else:
+    print("Wrong input")
 while is_continue:
-
-    pass
+    blackjack_game()
+    should_continue = input("Do you want to play BlackJack? type y to play and n to exit\n ")
+    if should_continue == "n":
+        is_continue = False
