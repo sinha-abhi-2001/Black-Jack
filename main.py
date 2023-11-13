c7eb78d7c047241decc18749f2e0ce8fc3bdb1ac
sinha-abhi-2001 <sinha00abhishek@gmail.com>
2023-11-13 13:14:54 +0530

    initial function and variables are defined

---
+++ main.py
@@ -0,0 +1,26 @@
+#BLACK JACK 
+import random
+card_values = {
+    'Ace': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'Jack': 10,
+    'Queen': 10,
+    'King': 10
+}
+def draw_cards():
+    pass
+is_continue = True
+should_continue = input("Do you want to play BlackJack? type y to play and n to exit\n ")
+if should_continue == "n":
+    is_continue = False
+while is_continue:
+
+    pass
